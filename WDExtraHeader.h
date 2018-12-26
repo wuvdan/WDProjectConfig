27daf4b039d3a1711e34b66b4439c714de8eb534
wudan <wudan@wudandebianyiqi.local>
2018-12-26 11:35:40 +0800

    提交代码

--- WDExtraHeader.h
+++ WDExtraHeader.h
@@ -83,13 +83,13 @@
 // 屏幕宽度
 #define kScreenWidth            [[UIScreen mainScreen] bounds].size.width
 // 状态栏高度
-#define kStatusBarHeight        (IS_iPhoneX ? 44.f : 20.f)
+#define kStatusBarHeight        (kIS_iPhoneX ? 44.f : 20.f)
 // 顶部导航栏高度
 #define kNavigationBarHeight    44.f
 // 状态栏高度 + 顶部导航栏高度
 #define kSafeAreaTopHeight      UIApplication.sharedApplication.statusBarFrame.size.height + 44
 // 底部安全距离
-#define kSafeAreaBottomHeight   (IS_iPhoneX ? 34.f : 0.f)
+#define kSafeAreaBottomHeight   (kIS_iPhoneX ? 34.f : 0.f)
 // Tabbar高度
 #define kTabbarHeight           49.f
 
--- WDProjectConfig.podspec
+++ WDProjectConfig.podspec
@@ -10,7 +10,7 @@ Pod::Spec.new do |s|
 
 
   s.name         = "WDProjectConfig"
-  s.version      = "1.0.8"
+  s.version      = "1.0.9"
   s.summary      = "iOS 项目搭建配置文件"
   s.description  = "为项目搭建提供简单的屏幕适配和其他简易使用的默认配置"
   s.homepage     = "https://github.com/wudan-ios/WDProjectConfig"
