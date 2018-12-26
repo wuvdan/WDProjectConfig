15017b423659abdb6f2b1f4fec78adf7e0e5228a
wudan <29372944+wudan-ios@users.noreply.github.com>
2018-12-26 11:01:47 +0800

    状态栏样式

---
+++ UINavigationController+WDStatusBar.m
@@ -0,0 +1,17 @@
+//
+//  UINavigationController+WDStatusBar.m
+//  WDProjectCongureDemo
+//
+//  Created by wudan on 2018/12/26.
+//  Copyright © 2018 wudan. All rights reserved.
+//
+
+#import "UINavigationController+WDStatusBar.h"
+
+@implementation UINavigationController (WDStatusBar)
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return [[self topViewController] preferredStatusBarStyle];
+}
+
+@end
