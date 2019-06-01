12cda7c10e8195ea99fbe499ceb2cdf42f2740b1
wudan <wudan@wudandebianyiqi-2.local>
2019-06-01 15:00:42 +0800

    提交代码

--- WDExtraHeader.h
+++ WDExtraHeader.h
@@ -77,7 +77,7 @@
 
 #pragma mark - 屏幕坐标、尺寸相关
 // 判断是否iPhone X
-#define kIS_iPhoneX             UIApplication.sharedApplication.statusBarFrame.size.height > 20 : YES : NO
+#define kIS_iPhoneX             (UIApplication.sharedApplication.statusBarFrame.size.height > 20 : YES : NO)
 // 屏幕高度
 #define kScreenHeight           [[UIScreen mainScreen] bounds].size.height
 // 屏幕宽度
@@ -87,9 +87,9 @@
 // 顶部导航栏高度
 #define kNavigationBarHeight    44.f
 // 状态栏高度 + 顶部导航栏高度
-#define kSafeAreaTopHeight      UIApplication.sharedApplication.statusBarFrame.size.height + 44
+#define kSafeAreaTopHeight      (UIApplication.sharedApplication.statusBarFrame.size.height + 44)
 // 底部安全距离
-#define kSafeAreaBottomHeight   (IS_iPhoneX ? 34.f : 0.f)
+#define kSafeAreaBottomHeight   (kIS_iPhoneX ? 34.f : 0.f)
 // Tabbar高度
 #define kTabbarHeight           49.f
 
